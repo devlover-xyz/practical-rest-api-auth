4e1b47dc01e4756baf63f1665103b7b98cdf367a
Aviq Baihaqy <aviq.baihaqy@gmail.com>
2020-10-11 16:29:50 +0700

    first commit

---
+++ api/protected.php
@@ -0,0 +1,56 @@
+<?php
+include_once '../config/database.php';
+require "../vendor/autoload.php";
+use \Firebase\JWT\JWT;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+$secret_key = "YOUR_SECRET_KEY";
+$jwt = null;
+$databaseService = new DatabaseService();
+$conn = $databaseService->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+$authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+
+$arr = explode(" ", $authHeader);
+
+$jwt = isset($arr[1])? $arr[1] : '';
+
+if($jwt){
+
+    try {
+
+        $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+
+        // Access is granted. Add code of the operation here 
+
+        echo json_encode(array(
+            "message" => "Access granted:",
+            "error" => false
+        ));
+
+    }catch (Exception $e){
+
+        http_response_code(401);
+
+        echo json_encode(array(
+            "message" => "Access denied.",
+            "error" => $e->getMessage()
+        ));
+    }
+
+}else{
+    // set response code
+    http_response_code(401);
+    
+    // tell the user access denied
+    echo json_encode(array("message" => "Access denied."));
+}
+?>
